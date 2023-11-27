f70ad92f71b7179441da11be71a5eb907ec07356
Thej0rdan <123018813+Thej0rdan@users.noreply.github.com>
2023-11-27 13:24:11 +0100

    Python Classwork

---
+++ dictionary.py
@@ -0,0 +1,4 @@
+d = dict()
+d= {'Age': 20
+    'height': 1.7
+    'color' : black}
---
+++ numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+a= [1,2,3,4]
+b = np.array(a) #valid
+b = np.array([1,2]) #valid
+
+#Creating a 2 by 2 matrix
+b = [1, 0, 0, 1] #declare a list
+a = np.array(b) #
+#arange with one r not arrange
+a = a.reshape(2,2)
